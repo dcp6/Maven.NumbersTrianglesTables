21ccfa7748b0acf4935467e23bf8151e1224dd87
Dan <dan@Zipcoders-MacBook-Pro.local>
2022-03-01 16:58:16 -0500

    copied files over 2

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -2,24 +2,40 @@ package io.zipcoder.microlabs.mastering_loops;
 
 
 public class NumberUtilities {
-    public static String getEvenNumbers(int start, int stop) {
-        return null;
+    public static String getEvenNumbers(int start, int stop)
+    {
+        String evenNumbersOutput = "";
+        for (Integer i = start; i < stop; i++ ) {
+            if (i % 2 == 0) {
+                evenNumbersOutput += String.valueOf(i);
+            }
+
+        }
+        return evenNumbersOutput;
+    }
+
+    public static String getOddNumbers(int start, int stop)
+    {
+        String oddNumbersOutput = "";
+        for (Integer i = start; i < stop; i++ ) {
+            if (i %2 == 1) {
+                oddNumbersOutput += String.valueOf(i);
+            }
+
+        }
+        return oddNumbersOutput;
     }
 
 
-    public static String getOddNumbers(int start, int stop) {
-        return null;
-    }
-
-
-    public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+    public static String getSquareNumbers(int start, int stop, int step)
+    {
+       return getExponentiations(start,stop,step,2);
     }
 
     public static String getRange(int start) {
+
         return null;
     }
-
     public static String getRange(int start, int stop) {
         return null;
     }
@@ -30,7 +46,13 @@ public class NumberUtilities {
     }
 
 
-    public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+    public static String getExponentiations(int start, int stop, int step, int exponent)
+    {
+        String exponentiationsOutput = "";
+        for (double i = start+=0.0; i < stop; i+= step) {
+            exponentiationsOutput += Math.round(Math.pow(i, exponent));
+        }
+        return exponentiationsOutput;
     }
 }
+
